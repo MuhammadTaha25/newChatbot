beac8ca284771b16b0a1a19425b2fbd768558e39
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-01-21 21:04:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -36,11 +36,11 @@ if audio_bytes:
         with open(webm_file_path, "wb") as f:
             f.write(audio_bytes)
         transcript = speech_to_text(webm_file_path)
-            if transcript:
-                st.session_state.messages.append({"role": "user", "content": transcript})
-                with st.chat_message("user"):
-                    st.write(transcript)
-                os.remove(webm_file_path)
+        if transcript:
+            st.session_state.messages.append({"role": "user", "content": transcript})
+            with st.chat_message("user"):
+                st.write(transcript)
+            os.remove(webm_file_path)
 
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
