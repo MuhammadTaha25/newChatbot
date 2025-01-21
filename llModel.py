1f70b87fd46b5b2a8d61c4ce2af703b2ba92adbf
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-01-21 19:22:27 +0500

    Update llModel.py

--- llModel.py
+++ llModel.py
@@ -18,7 +18,7 @@ def initialize_LLM(openai_api_key=None, gemini_api_key=None):
 
     if openai_api_key:
         try:
-            model_name = "gpt-4o-mini"
+            model_name = "gpt-3.5-turbo"
             LLM = ChatOpenAI(
                 model_name=model_name,
                 openai_api_key=openai_api_key,
