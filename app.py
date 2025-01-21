9a119f322e933343857e578551d3f8bd9783fafb
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-01-21 20:50:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,60 @@
-import streamlit as st
 from pineconedb import manage_pinecone_store
 from creating_chain import create_expert_chain
 from pineconedb import manage_pinecone_store
 from llModel import initialize_LLM
 import streamlit as st
+from utils import get_answer, text_to_speech, autoplay_audio, speech_to_text
+from audio_recorder_streamlit import audio_recorder
+from streamlit_float import *
+
+float_init()
 
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
+    
+def initialize_session_state():
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hi! How may I assist you today?"}
+        ]
+
+initialize_session_state()
+
+footer_container = st.container()
+with footer_container:
+    audio_bytes = audio_recorder()
 # Load environment variables from .env file
 
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
+if audio_bytes:
+    # Write the audio bytes to a file
+    with st.spinner("Transcribing..."):
+        webm_file_path = "temp_audio.mp3"
+        with open(webm_file_path, "wb") as f:
+            f.write(audio_bytes)
+        transcript = speech_to_text(webm_file_path)
+            if transcript:
+                st.session_state.messages.append({"role": "user", "content": transcript})
+                with st.chat_message("user"):
+                    st.write(transcript)
+                os.remove(webm_file_path)
+
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking🤔..."):
+            final_response = get_answer(st.session_state.messages)
+        with st.spinner("Generating audio response..."):    
+            audio_file = text_to_speech(final_response)
+            autoplay_audio(audio_file)
+        st.write(final_response)
+        st.session_state.messages.append({"role": "assistant", "content": final_response})
+        os.remove(audio_file)
+
+# Float the footer container and provide CSS to target it with
+footer_container.float("bottom: 0rem;")
 OPENAI_API_KEY =st.secrets['OPENAI_API_KEY']
 GOOGLE_API_KEY =st.secrets['google_api_key']
 
