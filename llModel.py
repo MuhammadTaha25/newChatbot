5392ab09ce8fae41ed803d68e20e6d9854ed5839
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-07-10 12:38:14 +0500

    Update llModel.py

--- llModel.py
+++ llModel.py
@@ -6,7 +6,7 @@ langsmith_tracing='true'
 langsmith_endpoint="https://api.smith.langchain.com"
 langsmith_api_key="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"
 langsmith_project="muskchatbot"
-OPENAI_API_KEY=st.secrets
+OPENAI_API_KEY =st.secrets['OPENAI_API_KEY']
 load_dotenv()
 def initialize_LLM(openai_api_key=None, gemini_api_key=None):
     """
