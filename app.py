4f887526783cb52fb9ffae6f4cc770d3c9a6b925
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-07-10 12:45:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from llModel import initialize_LLM
 import streamlit as st
 from streamlit_mic_recorder import speech_to_text
 
-langsmith_tracing='true'
-langsmith_endpoint="https://api.smith.langchain.com"
-langsmith_api_key="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"
-langsmith_project="muskchatbot"
-
+LANGSMITH_TRACING=true
+LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+LANGSMITH_API_KEY="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"
+LANGSMITH_PROJECT="pr-cooked-opportunity-37"
 OPENAI_API_KEY =st.secrets['OPENAI_API_KEY']
+
 GOOGLE_API_KEY =st.secrets['google_api_key']
 
 LLM=initialize_LLM(OPENAI_API_KEY,GOOGLE_API_KEY)
